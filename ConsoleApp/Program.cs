e46900b26828575a3c0c6fae8de73af729af3d8d
macuser301 <macuser30@yahoo.com>
2022-01-12 16:51:08 -0600

    initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       //variable declaration and initialization
+            int age = 41;
+            string name = "Rob";
+            bool isAlive = true;
+
+            string fullname = "Rob Davis";
+            int houseNumber = 130;
+            double myBalance = 10000.00;
+            decimal exactBalance = 10700.50m;
+            Console.WriteLine($"(name) (age) is he alive: (isAlive)");
+            Console.WriteLine($"(My balance is {myBalance},it is {exactBalance} in my account");
+        }
+    }
+}
